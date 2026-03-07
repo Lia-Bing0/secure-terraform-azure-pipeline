a555253390d258a8dec2d4f62c16bc0b0a444ab7
Lia Chandler <ktchandler92@gmail.com>
2026-03-07 16:01:53 -0500

    fix: remediate checkov policy violation in storage account configuration

--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "sa" {
 
   https_traffic_only_enabled      = true
   min_tls_version                 = "TLS1_2"
-  allow_nested_items_to_be_public = true
+  allow_nested_items_to_be_public = false
   # checkov:skip=CKV_AZURE_59: Temporary to allow GitHub-hosted runner to reach tfstate backend; will move to private endpoint + self-hosted runner
   public_network_access_enabled = true
   shared_access_key_enabled     = false
